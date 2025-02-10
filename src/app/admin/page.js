b07c0671f12550cface9a75f4b619b64ddeb62d0
vitor.bxr <vitor.bxr@gmail.com>
2025-02-10 23:00:53 +0000

    Criando estrutura do projeto, Iniciando o Painel de Administração com página inicial e produtos

---
+++ src/app/admin/page.js
@@ -0,0 +1,9 @@
+export default function AdminDashboard() {
+    return (
+      <div>
+        <h1>Painel de Administração</h1>
+        <p>Gerencie seus produtos, usuários e estoque.</p>
+      </div>
+    );
+  }
+  
---
+++ src/app/layout.js
@@ -0,0 +1,7 @@
+export default function RootLayout({ children }) {
+  return (
+    <html lang="pt-BR">
+      <body>{children}</body>
+    </html>
+  );
+}
